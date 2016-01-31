8378f2d3a866cf5367ba729055a60631fb8d4f20
rain84 <rainy_sky@mail.ru>
2016-01-31 19:39:39 +0300

    Week 1. Lesson 4

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,74 @@
-angular.module('starter.controllers', [])
+angular.module( 'conFusion.controllers', [] )
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller( 'AppCtrl', function ( $scope, $ionicModal, $timeout, $ionicGesture ) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    var $h1 = angular.element( document.querySelector( 'h1' ) );
+    $ionicGesture.on( 'tap',
+                      function () {
+                        console.log( 'on-drag-left' );
+                      }, $h1
+    );
+    ionic.on( 'click',
+              function () {
+                console.log( 'click' );
+              }, document.querySelector( 'h1' )
+    );
+    ionic.on( 'tap',
+              function () {
+                console.log( 'click' );
+              }, document.querySelector( 'h1' )
+    );
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    //console.log( 'ionic : %O', ionic );
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl( 'templates/login.html', {
+      scope : $scope
+    } ).then( function ( modal ) {
+      $scope.modal = modal;
+    } );
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log( 'Doing login', $scope.loginData );
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout( function () {
+        $scope.closeLogin();
+      }, 1000 );
+    };
+  } )
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+  .controller( 'PlaylistsCtrl', function ( $scope ) {
+    $scope.playlists = [
+      { title : 'Reggae', id : 1 },
+      { title : 'Chill', id : 2 },
+      { title : 'Dubstep', id : 3 },
+      { title : 'Indie', id : 4 },
+      { title : 'Rap', id : 5 },
+      { title : 'Cowbell', id : 6 }
+    ];
+  } )
+
+  .controller( 'PlaylistCtrl', function ( $scope, $stateParams ) {
+  } );
